89ca34a2e016a94533d6520c6d0cf6d5bd594df6
Kiko Carisse <kiko@mediabot.ca>
2023-04-21 05:46:42 -0600

    adding deployment steps for ec2

---
+++ README.md
@@ -0,0 +1,26 @@
+# deploying and startup on new ec2 server
+
+REQUIRES: ubuntu 20.04 ec2 t2.small instance (defaults on the rest)
+
+make sure your domain is pointed at the server's ip address before trying to do the certbot let's encrypt ssl step
+
+## Connect to Instance and Download Repo, Then Execute Server Setup Bash File
+
+```
+ssh -i "C:\Users\kiko\.ssh\kikos_aws\aws_mth_key.pem" ubuntu@ec2-3-99-168-149.ca-central-1.compute.amazonaws.com
+
+# once connected, execute the following
+sudo -u
+apt update && apt upgrade -y
+cd /home/ubuntu/
+mkdir apps
+cd apps
+mkdir empty-hearts-app
+cd empty-hearts-app
+git clone https://github.com/kikoking1/empty-hearts.git
+bash /home/ubuntu/apps/empty-hearts-app/empty-hearts/devops/new-ubuntu-server-setup.sh
+```
+
+NOTE: the only prompts should be the certbot prompts at the very end.
+
+Put in a real email, say yes twice, and it should be live
---
+++ devops/new-ubuntu-server-setup.sh
@@ -0,0 +1,80 @@
+#install node and npm
+cd /home/ubuntu/
+curl -sL https://deb.nodesource.com/setup_19.x -o /tmp/nodesource_setup.sh
+bash /tmp/nodesource_setup.sh
+rm /tmp/nodesource_setup.sh
+apt-get install -y nodejs
+
+#build react app
+cd /home/ubuntu/apps/empty-hearts-app/empty-hearts/client
+npm i
+npm run build
+
+#install nginx and configure to serve react app. Also configure nginx to proxy api calls to dotnet app. Then start nginx
+apt install nginx -y
+systemctl enable nginx
+cd /etc/nginx/sites-available
+touch empty-hearts.com
+cat > empty-hearts.com <<'endmsg'
+server {
+        listen 80;
+        listen [::]:80;
+
+         root /home/ubuntu/apps/empty-hearts-app/empty-hearts/client/build;
+
+        # Add index.php to the list if you are using PHP
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name empty-hearts.com www.empty-hearts.com;
+
+        location / {
+                try_files $uri /index.html;
+        }
+
+         location /api {
+            proxy_pass http://localhost:5289;
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Connection 'upgrade';
+            proxy_set_header Host $host;
+            proxy_cache_bypass $http_upgrade;
+        }
+}
+endmsg
+ln -s /etc/nginx/sites-available/empty-hearts.com /etc/nginx/sites-enabled/
+systemctl restart nginx
+
+#install dotnet 7.0 and build dotnet app. Then configure dotnet app to run locally as a service. Then start dotnet app service
+cd /home/ubuntu/apps/empty-hearts-app/empty-hearts/server/
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+apt-get update && apt-get install -y dotnet-sdk-7.0
+dotnet publish --configuration Release
+
+cd /home/ubuntu/apps/empty-hearts-app/empty-hearts/server/MTT.API/bin/Release/net7.0/publish/
+touch /etc/systemd/system/empty-hearts.service
+
+cat > /etc/systemd/system/empty-hearts.service <<'endmsg'
+Description=Empty Hearts Dotnet App
+[Service]
+WorkingDirectory=/home/ubuntu/apps/empty-hearts-app/empty-hearts/server/MTT.API/bin/Release/net7.0/publish/
+ExecStart=/usr/bin/dotnet /home/ubuntu/apps/empty-hearts-app/empty-hearts/server/MTT.API/bin/Release/net7.0/publish/MTT.API.dll
+Restart=always
+# Restart service after 10 seconds if the dotnet service crashes:
+RestartSec=10
+KillSignal=SIGINT
+SyslogIdentifier=empty-hearts-dotnet-app
+User=root
+Environment=ASPNETCORE_ENVIRONMENT=Production
+Environment=DOTNET_PRINT_TELEMETRY_MESSAGE=false
+[Install]
+WantedBy=multi-user.target
+endmsg
+systemctl enable empty-hearts.service
+systemctl start empty-hearts.service
+
+#install certbot ssl let's encrypt certificate
+snap install --classic certbot
+ln -s /snap/bin/certbot /usr/bin/certbot
+certbot --nginx -d empty-hearts.com
