cd0b5ccc9ff8c6b48679fb3425b15c1d6177856b
Kiko Carisse <kiko@mediabot.ca>
2023-04-21 05:55:26 -0600

    updating domain for local and production

--- client/src/util/axios.js
+++ client/src/util/axios.js
@@ -1,5 +1,8 @@
 import axios from "axios";
-const BASE_URL = "https://localhost:7083";
+const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "https://localhost:7083"
+    : "https://empty-hearts.com";
 
 export default axios.create({
   baseURL: BASE_URL,
